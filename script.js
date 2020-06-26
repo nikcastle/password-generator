ca5c3335b7335741e915ead6b23d431dea79d27e
Lady Stock <nikki.castlea@gmail.com>
2020-06-26 17:01:40 -0600

    defined the variables

--- script.js
+++ script.js
@@ -1,5 +1,35 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var clipboardBtn = document.querySelector(#clipboard);
+var lowercaseLetters = "abcdefghijklmnopqrstuvwxyz".split("");
+var uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+var numbers = "1234567890".split("");
+var special = "!@#$%&*".split("");
+
+var userChars = [...letters, ...numbers];
+
+// * series of confirms to determine what types of characters user wants to use
+var setLength = prompt("How many characters would you like to inlcude in the password?")
+var confirmNumbers = confirm("Do you want to use numbers?");
+var confirmUppercase = confirm("Do you want to use uppercase letters?");
+var confirmLowercase = confirm("Do you want to use lowercase letters?");
+var confirmSpecial = confirm ("Do you want to use special characters?");
+//etc
+
+if (confirmNumbers) {
+    userChars = [userChars, ... numbers];
+}
+if (confirmUppercase) {
+    userChars = [userChars, ...numbers, ...uppercaseLetters];
+    //confirmUppercase
+}
+if (confirmLowercase) {
+    userChars = [userChars, ...numbers, ...uppercaseLetters, ...lowercaseLetters];
+    //confirmSpecial
+}
+if (confirmSpecial) {
+    userChars = [userChars, ...numbers, ... uppercaseLetters, ...lowercaseLetters, ...special];
+}
 
 // Write password to the #password input
 function writePassword() {
@@ -8,7 +38,20 @@ function writePassword() {
 
   passwordText.value = password;
 
+
+  for(var i = 0; i<= ; i++) {
+      password = password + values.charAt(Math.floor(Math.random() * userChars.length))
+
+  }
+
+}
+
+function copyClipboard() {
+    var copyText = document.getElementById("password")
+    copyText.select();
+    document.execCommand("copy");
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+clipboardBtn.addEventListener("click", copyClipboard);
