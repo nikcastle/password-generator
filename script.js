3e82087fbd99ab4f3e8f62430fd38e080c08e7d7
Lady Stock <nikki.castlea@gmail.com>
2020-06-26 18:42:37 -0600

    moved confirms to inside of generate function

--- script.js
+++ script.js
@@ -1,50 +1,58 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var clipboardBtn = document.querySelector(#clipboard);
+var clipboardBtn = document.querySelector("#clipboard");
 var lowercaseLetters = "abcdefghijklmnopqrstuvwxyz".split("");
 var uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
 var numbers = "1234567890".split("");
 var special = "!@#$%&*".split("");
 
-var userChars = [...letters, ...numbers];
+var userChars = [];
 
 // * series of confirms to determine what types of characters user wants to use
-var setLength = prompt("How many characters would you like to inlcude in the password?")
-var confirmNumbers = confirm("Do you want to use numbers?");
-var confirmUppercase = confirm("Do you want to use uppercase letters?");
-var confirmLowercase = confirm("Do you want to use lowercase letters?");
-var confirmSpecial = confirm ("Do you want to use special characters?");
 //etc
 
-if (confirmNumbers) {
-    userChars = [userChars, ... numbers];
-}
-if (confirmUppercase) {
-    userChars = [userChars, ...numbers, ...uppercaseLetters];
-    //confirmUppercase
-}
-if (confirmLowercase) {
-    userChars = [userChars, ...numbers, ...uppercaseLetters, ...lowercaseLetters];
-    //confirmSpecial
-}
-if (confirmSpecial) {
-    userChars = [userChars, ...numbers, ... uppercaseLetters, ...lowercaseLetters, ...special];
-}
+
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-
-  for(var i = 0; i<= ; i++) {
-      password = password + values.charAt(Math.floor(Math.random() * userChars.length))
-
-  }
-
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+    
+    passwordText.value = password;
+    
 }
+
+function generatePassword () {
+    var password = ""
+    var setLength = prompt("How many characters would you like to include in the password? \n(Please enter a number between 8 and 128 characters)")
+    var confirmNumbers = confirm("Do you want to use numbers?");
+    var confirmUppercase = confirm("Do you want to use uppercase letters?");
+    var confirmLowercase = confirm("Do you want to use lowercase letters?");
+    var confirmSpecial = confirm ("Do you want to use special characters?");
+    
+
+       if (confirmNumbers) {
+           userChars = [...userChars, ... numbers];
+       }
+       if (confirmUppercase) {
+           userChars = [...userChars, ...uppercaseLetters];
+           //confirmUppercase
+       }
+       if (confirmLowercase) {
+           userChars = [...userChars, ...lowercaseLetters];
+           //confirmSpecial
+       }
+       if (confirmSpecial) {
+           userChars = [...userChars, ...special];
+       }
+       for(var i = 0; i<setLength; i++) {
+           password = password + userChars[Math.floor(Math.random() * userChars.length)]
+           
+       }
+       return password;
+  
+   }
+
 
 function copyClipboard() {
     var copyText = document.getElementById("password")
